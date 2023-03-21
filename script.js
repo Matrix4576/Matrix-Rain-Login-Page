11c084891fb05be368a470f882122830b82cea63
Agniva Roy <77002089+Matrix4576@users.noreply.github.com>
2023-03-21 17:55:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,33 +6,35 @@ const katakana = '私たちは愛する見知らぬ人ではありませんあ�
 const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-_=+{}[]|;:,<.>/?';
 const nums = '0123456789';
 const alphabet = katakana + latin + nums;
-const fontSize = 14;
+const fontSize = 10;
 const columns = canvas.width / fontSize;
 const signinBtn = document.querySelector('.signin');
 const signupBtn = document.querySelector('.signup');
+const formbg = document.querySelector('.formbG')
 const rainDrops = [];
 for (let x = 0; x < columns; x++) {
-    rainDrops[x] = 1;
+    rainDrops[x] = 0;
 }
 const draw = () => {
     context.fillStyle = 'rgba(0, 0, 0, 0.05)';
-    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.fillRect(0, 1, canvas.width, canvas.height);
     context.fillStyle = '#00FF41';
     context.font = fontSize + 'px monospace';
     for (let i = 0; i < rainDrops.length; i++) {
         const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
         context.fillText(text, i * fontSize, rainDrops[i] * fontSize);
-        if (rainDrops[i] * fontSize > canvas.height && Math.random() > 0.975) {
+        if (rainDrops[i] * fontSize > canvas.height && Math.random() > 0.969) {
             rainDrops[i] = 0;
         }
         rainDrops[i]++;
     }
 };
-setInterval(draw, 60);
+setInterval(draw, 40);
 signupBtn.onclick = function () {
-    formbG.classList.add('left')
+    formbg.classList.remove('right')
+    formbg.classList.add('left')
 }
 signinBtn.onclick = function () {
-    formbG.classList.remove('left')
-    formbG.classList.add('right')
+    formbg.classList.remove('left')
+    formbg.classList.add('right')
 }
